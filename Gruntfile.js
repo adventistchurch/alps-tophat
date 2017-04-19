b4d1570a79ddc895762ff2721d09b7942dd03ed0
Kelsey Cahill <kcahill@southleft.com>
2017-04-19 14:04:58 -0400

    Added gruntfile and updated to produce CDN folder for production

---
+++ Gruntfile.js
@@ -0,0 +1,165 @@
+module.exports = function(grunt) {
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    var pkg = grunt.file.readJSON('package.json');
+
+    /**
+     * Major Version and Version Number
+     *
+     * @description
+     * Increase the version number to the desired version for css and javascript
+     * files. This will also create a version directory in the /cdn folder in the structure
+     * of /cdn/<major_version/<version>/ that contains the javascript and css.
+     */
+    var major_version = "2";
+    var version = "2.0.5";
+
+    grunt.initConfig({
+        pkg: pkg,
+        version: version,
+        major_version: major_version,
+
+        mkdir: {
+            prod: {
+                options: {
+                    mode: 0777,
+                    create: ['cdn/<%= major_version %>/<%= version %>/css']
+                }
+            }
+        },
+
+        sass: {
+            dev: {
+                options: {
+                    outputStyle: 'nested',
+                    sourceMap: true,
+                },
+                files: {
+                    'tophat.css': 'source/sass/topcat.scss'
+                }
+            },
+            prod: {
+                options: {
+                    outputStyle: 'compressed',
+                    sourceMap: false,
+                },
+                files: {
+                    'cdn/<%= major_version %>/<%= version %>/css/tophat.css': 'source/sass/tophat.scss',
+                }
+            }
+        },
+
+        copyFiles: '**/*.{eot,svg,ttf,woff,pdf}',
+
+        copy: {
+            dev: {
+                files: [
+                    // includes files within path
+                    {
+                        expand: true,
+                        cwd: 'source/',
+                        src: ['<%= copyFiles %>'],
+                        dest: '/',
+                        filter: 'isFile'
+                    }
+                ]
+            },
+            prod: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'source/',
+                        src: ['<%= copyFiles %>'],
+                        dest: 'cdn/<%= major_version %>/<%= version %>/'
+                    }
+                ]
+            }
+        },
+
+        // Symlink creates a cdn/latest
+        symlink: {
+            options: {
+                overwrite: true
+            },
+            expanded: {
+                files: [
+                    // All child directories in "source" will be symlinked into the "build"
+                    // directory, with the leading "source" stripped off.
+                    {
+                        src: ['cdn/<%= major_version %>/<%= version %>'],
+                        dest: 'cdn/<%= major_version %>/latest',
+                        filter: 'isDirectory'
+                    }
+                ]
+            },
+        },
+
+        // Watch options: what tasks to run when changes to files are saved
+        watch: {
+            options: {},
+            html: {
+                files: ['source/**/*.html'], // Watch for changes to these html files to run htmlhint (validation) task
+                tasks: ['css'],
+                options: {
+                    spawn: false
+                }
+            },
+            css: {
+                files: ['source/sass/*.scss'],
+                tasks: ['css'] // Compile with Compass when Sass changes are saved
+            },
+            copy: {
+                files: ['source/<%= copyFiles %>'],
+                tasks: ['copy']
+            }
+        }
+
+    });
+
+    /**
+     * CSS tasks
+     */
+    grunt.registerTask('css', [
+        'sass:dev'
+    ]);
+
+    /**
+     * Dev tasks
+     */
+    grunt.registerTask('dev', [
+        'copy:dev',
+        'css'
+    ]);
+
+    /**
+     * Production tasks
+     */
+    grunt.registerTask('prod', [
+        'mkdir:prod',
+        'sass:prod',
+        'symlink',
+        'copy:prod'
+    ]);
+
+    /**
+     * DeployBot Task
+     */
+    grunt.registerTask('deploybot', [
+        // 'css',
+        'sass:dev',
+        'copy'
+    ]);
+
+
+
+    /**
+     * Default Tasks
+     */
+    grunt.registerTask('default', ['dev', 'watch']);
+
+    /**
+     * Staging Tasks
+     */
+    grunt.registerTask('staging', ['dev']);
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# tophat
+# ALPS – Tophat
+
+
+### Getting Started
+
+- [Install node globally](https://docs.npmjs.com/getting-started/installing-node)
+- run `npm install` (may need to be run as `sudo`)
+- run `grunt`
+- run `grunt prod` for production cdn files
+
+
+## Project Standards
+
+### Front-End General Best Practices
+1. Separation of presentation, content, and behavior.
+2. Markup should be well-formed, semantically correct, and generally valid (if possible).
+3. Javascript should progressively enhance the experience.
+4. Always keep performance at top of mind when coding.
+
+### CSS Coding Standards
+The CSS in this project follows [Harry Robert's](http://csswizardry.com/) [itcss](http://itcss.io/) architecture. [CSS/SCSS Coding Best Practices](http://cssguidelin.es/) should be the referenced standards guide when writing sane, manageable, scalable CSS.
+
+### Git Commit Messages
+All commit messages should abide by the following criteria:
+
+* Begin with a one line summary. It should be short (50 chars or less).
+* This should be followed by a longer description if necessary.
+* The first two items should be separated by an empty line.
+* All lines should be wrapped at approximately 72 characters.
+* Reference an issue in your commits whenever possible. Use the [Github standard](https://help.github.com/articles/closing-issues-via-commit-messages/) ```gh-34``` for ticket ```#34```.
+* If you forget to reference the issue in your commit, and the commit has already been pushed, reference the commit hash in a comment on the ticket.
+
+### Troubleshooting Installs
+
+Make sure you are running your terminal/command line session as administrator. This could mean `sudo`, or opening the window with a right-click option.
