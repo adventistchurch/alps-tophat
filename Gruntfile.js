56a4022a0883a04ea9db6d5607d3ad5caae46272
Brent Hardinge <designerbrent@gmail.com>
2017-05-05 17:11:45 -0400

    fixing the paragraph margin, adding Montserrat font, adding a narrow__hide class and header__top-hat-01

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
      * of /cdn/<major_version/<version>/ that contains the javascript and css.
      */
     var major_version = "2";
-    var version = "2.0.5";
+    var version = "2.0.0-beta";
 
     grunt.initConfig({
         pkg: pkg,
