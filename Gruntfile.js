94035830e4d4bcf79ce11f43217e52ec95dff753
Brent Hardinge <designerbrent@gmail.com>
2017-05-09 13:30:38 -0400

    renaming the 'top-hat' variable to 'tophat'

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
      * of /cdn/<major_version/<version>/ that contains the javascript and css.
      */
     var major_version = "2";
-    var version = "2.0.0-beta";
+    var version = "2.0.0";
 
     grunt.initConfig({
         pkg: pkg,
